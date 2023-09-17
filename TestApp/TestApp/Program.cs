d40d1c06d50bd19f71e3b952d4dcf3f61aaee501
Taha <Taha@gmail.com>
2023-09-17 13:26:37 +0300

    Instryuctions For Git

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,5 +1,14 @@
 ﻿/*
-    Inital Lines Of Code
-        Another Line
-        Another Line
+ * 
+ * https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+ * 
+    - Install Git 
+    - Configuration UserName and Email
+    - git init
+    - Create GitHub Account 
+    - Create Github repo
+    - git add remote origin 
+    - 
+    - git push -u origin master
+
  */
